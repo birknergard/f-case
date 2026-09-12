4c2698c88a924bbf3f204195aa6cb888603a4173
Birk Nergaard <birknergard@gmail.com>
2026-09-12 23:45:30 +0200

    feat: added logger

--- backend/src/main/java/com/fcase/Controllers/FacilityController.java
+++ backend/src/main/java/com/fcase/Controllers/FacilityController.java
@@ -1,5 +1,7 @@
 package com.fcase.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/facility")
 public class FacilityController {
+  private static final Logger log = LoggerFactory.getLogger(FacilityController.class);
 
   @GetMapping("/{msg}")
   public ResponseEntity<String> greeting(@PathVariable String msg) {
+    log.info("Pinged server");
     return ResponseEntity.ok(String.format("HELLO %s!", msg));
   }
 }
